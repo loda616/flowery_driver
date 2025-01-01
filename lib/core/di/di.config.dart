ecf03958225f8bde9191b631acec1575c5381677
RaWan Magdy <rawan.magdy.fahmy@gmail.com>
2025-01-01 22:13:44 +0200

    TRAC-1 Finish MyProfile

---
+++ lib/core/di/di.config.dart
@@ -0,0 +1,67 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:dio/dio.dart' as _i361;
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+
+import '../../data/api/profile_api/profile_api_manager.dart' as _i592;
+import '../../data/data_source/remote_data_source/profile/profile_remote_data_source.dart'
+    as _i53;
+import '../../data/data_source/remote_data_source/profile/profile_remote_data_source_impl.dart'
+    as _i466;
+import '../../data/repository/profile/profile_repository_impl.dart' as _i181;
+import '../../domain/repository/profile/profile_repository.dart' as _i742;
+import '../../domain/use_case/profile/get_logged_driver_info_use_case.dart'
+    as _i337;
+import '../../domain/use_case/profile/get_vehicle_info_use_case.dart' as _i897;
+import '../../domain/use_case/profile/logout_use_case.dart' as _i147;
+import '../../presentation/profile/view_model/profile_view_model.dart' as _i671;
+import '../api/dio/dio_factory.dart' as _i763;
+import '../api/dio/dio_module.dart' as _i223;
+
+extension GetItInjectableX on _i174.GetIt {
+// initializes the registration of main-scope dependencies inside of GetIt
+  _i174.GetIt init({
+    String? environment,
+    _i526.EnvironmentFilter? environmentFilter,
+  }) {
+    final gh = _i526.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    final dioModule = _$DioModule();
+    gh.factory<_i763.DioFactory>(() => _i763.DioFactory());
+    gh.lazySingleton<_i361.Dio>(() => dioModule.dio);
+    gh.lazySingleton<_i592.ProfileApiManager>(
+        () => _i592.ProfileApiManager(gh<_i361.Dio>()));
+    gh.factory<_i53.ProfileRemoteDataSource>(() =>
+        _i466.ProfileRemoteDataSourceImpl(
+            apiManger: gh<_i592.ProfileApiManager>()));
+    gh.factory<_i742.ProfileRepository>(() => _i181.ProfileRepositoryImpl(
+        profileRemoteDataSource: gh<_i53.ProfileRemoteDataSource>()));
+    gh.factory<_i147.LogoutUseCase>(
+        () => _i147.LogoutUseCase(gh<_i742.ProfileRepository>()));
+    gh.factory<_i337.GetLoggedDriverInfoUseCase>(() =>
+        _i337.GetLoggedDriverInfoUseCase(
+            profileRepository: gh<_i742.ProfileRepository>()));
+    gh.factory<_i897.GetVehicleInfoUseCase>(() => _i897.GetVehicleInfoUseCase(
+        profileRepository: gh<_i742.ProfileRepository>()));
+    gh.factory<_i671.ProfileCubit>(() => _i671.ProfileCubit(
+          gh<_i337.GetLoggedDriverInfoUseCase>(),
+          gh<_i147.LogoutUseCase>(),
+          gh<_i897.GetVehicleInfoUseCase>(),
+        ));
+    return this;
+  }
+}
+
+class _$DioModule extends _i223.DioModule {}
---
+++ lib/data/api/profile_api/profile_api_manager.g.dart
@@ -0,0 +1,164 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile_api_manager.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations
+
+class _ProfileApiManager implements ProfileApiManager {
+  _ProfileApiManager(
+    this._dio, {
+    this.baseUrl,
+    this.errorLogger,
+  }) {
+    baseUrl ??= 'https://flower.elevateegy.com/';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  final ParseErrorLogger? errorLogger;
+
+  @override
+  Future<DriverDataResponseModel?> getLoggedDriverInfo(String token) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{r'Authorization': token};
+    _headers.removeWhere((k, v) => v == null);
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<DriverDataResponseModel>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          'api/v1/drivers/profile-data',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>?>(_options);
+    late DriverDataResponseModel? _value;
+    try {
+      _value = _result.data == null
+          ? null
+          : DriverDataResponseModel.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<VehicleResponseModel?> getVehicleInfo(
+    String token,
+    String vehicleId,
+  ) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{r'Authorization': token};
+    _headers.removeWhere((k, v) => v == null);
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<VehicleResponseModel>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          'api/v1/vehicles',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>?>(_options);
+    late VehicleResponseModel? _value;
+    try {
+      _value = _result.data == null
+          ? null
+          : VehicleResponseModel.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<String?> logout(String token) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{r'Authorization': token};
+    _headers.removeWhere((k, v) => v == null);
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<String>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          'api/v1/drivers/logout',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<String>(_options);
+    late String? _value;
+    try {
+      _value = _result.data;
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+
+  String _combineBaseUrls(
+    String dioBaseUrl,
+    String? baseUrl,
+  ) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+  }
+}
